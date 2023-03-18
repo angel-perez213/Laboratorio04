097d190d0698e901b5b4195edc38a8a759c0274c
ANGEL PEREZ CRUZ <aperezcruz@correo.url.edu.gt>
2023-03-18 10:36:49 -0700

    Resolver ejercicio 1

--- Laboratorio04/Program.cs
+++ Laboratorio04/Program.cs
@@ -4,9 +4,9 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Nombre: ");
-            Console.WriteLine("Carnet: ");
-            Console.WriteLine("Seccion: ");
+            Console.WriteLine("Nombre: Angel Samuel Pérez");
+            Console.WriteLine("Carnet: 1135323");
+            Console.WriteLine("Seccion: 04");
         }
     }
 }
